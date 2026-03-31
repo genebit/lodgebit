aad0722d39997852226650540d64c53c073636a8
Johcel Gene T. Bitara • BS IT <genebitara@gmail.com>
2026-03-31 21:48:43 +0800

    fix(deploy): next build.

--- src/components/admin/PageHero.tsx
+++ src/components/admin/PageHero.tsx
@@ -14,8 +14,8 @@ export default function PageHero({ heading, leadingText, className, ...props }: 
       {...props}
     >
       <h1 className="text-2xl font-bold tracking-tight">{heading}</h1>
-      {leadingText && <p className="text-muted-foreground mt-1">{leadingText}</p>}
-      <div className="absolute -right-48 -bottom-16 scale-[0.4]">
+      {leadingText && <p className="text-muted-foreground mt-1 z-20">{leadingText}</p>}
+      <div className="absolute -right-48 -bottom-16 scale-[0.4] z-0">
         <svg width="688" height="204" viewBox="0 0 688 204" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M116.491 0H461.991L643.491 203H240.491L116.491 0Z" fill="#4B39B0" />
           <path d="M0 203.5L117.491 0L242.491 203.5H0Z" fill="#1A265A" />
